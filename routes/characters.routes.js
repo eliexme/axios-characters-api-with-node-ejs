8728bd9bdbbf75ddc1ce7e92ecf071e212adc6f1
Elisa Exposito <eliexme@gmail.com>
2023-05-12 17:02:52 +0100

    done to update

--- routes/characters.routes.js
+++ routes/characters.routes.js
@@ -11,6 +11,26 @@ router.get("/characters", (req, res, next) => {
     .catch(err => console.error(err))
 });
 
+router.get('/characters/create', async(req, res, next) =>{
+    res.render('characters/create-character')
+})
+
+router.post('/characters/create', async(req, res, next) =>{
+    try {
+        if(req.body.debt === 'on'){
+            req.body.debt = true
+        } else{
+            req.body.debt = false
+        }
+        console.log(req.body)
+    const response = await axios.post("https://ih-crud-api.herokuapp.com/characters", {name: req.body.name, occupation: req.body.occupation, weapon: req.body.weapon, debt: req.body.debt})
+    res.redirect('/characters')
+
+    } catch (error) {
+        console.log(error)
+    }
+    
+})
 
 router.get("/characters/:id", (req, res, next) => {
     axios.get(`https://ih-crud-api.herokuapp.com/characters/${req.params.id}`)
@@ -20,6 +40,17 @@ router.get("/characters/:id", (req, res, next) => {
     })
     .catch(err => console.error(err))
 });
+
+
+router.get('/characters/:id/edit', async(req, res, next) =>{
+    const characterData = await axios.get(`https://ih-crud-api.herokuapp.com/characters/${req.params.id}`)
+    const updateChar = await axios.put("https://ih-crud-api.herokuapp.com/characters", {})
+    res.render('characters/edit-character')
+})
+
+router.post('/characters/:id/edit', async(req, res, next) =>{
+    
+})
 
 module.exports = router;
 
